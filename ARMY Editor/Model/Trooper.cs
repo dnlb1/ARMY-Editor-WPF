afdaa0b19dffcdfb9237b0e8801ccc18cca20a5c
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 14:38:47 +0100

    Trooper model added

---
+++ ARMY Editor/Model/Trooper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARMY_Editor.Model
+{
+    public enum Side
+    {
+        Good,
+        Evil,
+        Neutral
+    }
+    class Trooper
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int power;
+
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+
+        private double speed;
+
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        private Side side;
+
+        public Side Side
+        {
+            get { return side; }
+            set { side = value; }
+        }
+
+
+    }
+}
