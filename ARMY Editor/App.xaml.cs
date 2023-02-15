c01929f51c42aef8dc69fef0313d965368884195
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 15:29:56 +0100

    IOC App xml added with singletons

--- ARMY Editor/App.xaml.cs
+++ ARMY Editor/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ARMY_Editor.Logic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +17,12 @@ namespace ARMY_Editor
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Ioc.Default.ConfigureServices(new ServiceCollection()
+                .AddSingleton<IWarLogic, WarLogic>()
+                .AddSingleton<IMessenger>(WeakReferenceMessenger.Default)  
+                .BuildServiceProvider());
+        }
     }
 }
