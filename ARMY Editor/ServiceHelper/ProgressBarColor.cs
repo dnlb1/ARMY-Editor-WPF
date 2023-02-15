131e116a0c51ac68f93ae73299212dbf724c55da
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 15:12:03 +0100

    ValueConverter created

---
+++ ARMY Editor/ServiceHelper/ProgressBarColor.cs
@@ -0,0 +1,36 @@
+﻿using ARMY_Editor.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ARMY_Editor.ServiceHelper
+{
+    class ProgressBarColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Side s = (Side)Enum.Parse(typeof(Side), value.ToString());
+            switch (s)
+            {
+                case Side.Good:
+                    return Brushes.Green;
+                case Side.Evil:
+                    return Brushes.Red;
+                case Side.Neutral:
+                    return Brushes.Wheat;
+                default:
+                    return Brushes.Wheat;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
