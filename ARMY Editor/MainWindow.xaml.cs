4e676eec5e93e343c31ef323b35918227312af5d
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 17:41:43 +0100

    Json seri/desi created

--- ARMY Editor/MainWindow.xaml.cs
+++ ARMY Editor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ARMY_Editor.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace ARMY_Editor
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            (this.DataContext as MainWindowViewModel).SaveJson();
         }
     }
 }
--- ARMY Editor/ViewModel/MainWindowViewModel.cs
+++ ARMY Editor/ViewModel/MainWindowViewModel.cs
@@ -3,10 +3,12 @@ using ARMY_Editor.Model;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.DependencyInjection;
 using Microsoft.Toolkit.Mvvm.Input;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -65,8 +67,6 @@ namespace ARMY_Editor.ViewModel
             set { SetProperty(ref avgpower, value); }
         }
 
-
-
         IWarLogic logic;
 
         public static bool IsInDesigner
@@ -86,15 +86,28 @@ namespace ARMY_Editor.ViewModel
         public MainWindowViewModel(): this(IsInDesigner ? null : Ioc.Default.GetService<IWarLogic>())
         {
         }
-
-
         public MainWindowViewModel(IWarLogic logic)
         {
             this.logic = logic;
-            this.Camp = new ObservableCollection<Trooper>();
-            this.Camp.Add(new Trooper() { Side = Side.Neutral, Speed = 2, Name = "Bela", Power = 4});
-            this.War = new ObservableCollection<Trooper>();
-            this.War.Add(new Trooper() { Side = Side.Evil, Speed = 2, Name = "Bela", Power = 4 });
+            if (File.Exists("TrooperCamp.json"))
+            {
+                string jsonString = File.ReadAllText("TrooperCamp.json");
+                Camp = JsonConvert.DeserializeObject<ObservableCollection<Trooper>>(jsonString);
+            }
+            else
+            {
+                this.Camp = new ObservableCollection<Trooper>();
+            }
+
+            if (File.Exists("TrooperWar.json"))
+            {
+                string jsonString = File.ReadAllText("TrooperWar.json");
+                War = JsonConvert.DeserializeObject<ObservableCollection<Trooper>>(jsonString);
+            }
+            else
+            {
+                this.War = new ObservableCollection<Trooper>();
+            }
 
             logic.SetupLogic(War,Camp);
 
@@ -140,6 +153,15 @@ namespace ARMY_Editor.ViewModel
                 OnPropertyChanged("AvgSpeed");
                 OnPropertyChanged("AvgPower");
             });
+
+        }
+
+        public void SaveJson()
+        {
+            string JsonCamp = JsonConvert.SerializeObject(Camp);
+            string JsonWar = JsonConvert.SerializeObject(War);
+            File.WriteAllText("TrooperCamp.json", JsonCamp);
+            File.WriteAllText("TrooperWar.json", JsonWar);
 
         }
     }
