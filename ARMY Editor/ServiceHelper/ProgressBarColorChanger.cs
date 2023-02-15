58a5af3cc78410013af3125f03cca8eca127d635
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 15:20:13 +0100

    ProgressBar color changer created

---
+++ ARMY Editor/ServiceHelper/ProgressBarColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ARMY_Editor.ServiceHelper
+{
+    class ProgressBarColorChanger : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double n = (double)Double.Parse(value.ToString());
+            if (n < 4)
+            {
+                return Brushes.Red;
+            }
+            else if (n < 7)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.Green;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
