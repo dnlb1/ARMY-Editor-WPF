b151ddd46ecb78fa1da3bc0d3fe6639cd627ed71
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 16:10:02 +0100

    Trooper Window/VM created

---
+++ ARMY Editor/ViewModel/TrooperCreatorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ARMY_Editor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARMY_Editor.ViewModel
+{
+    class TrooperCreatorViewModel
+    {
+    }
+}
