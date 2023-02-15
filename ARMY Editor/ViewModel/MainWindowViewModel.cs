a1977aa0ebbb56b53c5d2026a17a91b9dc8846b5
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 15:22:01 +0100

    Button commands added

--- ARMY Editor/ViewModel/MainWindowViewModel.cs
+++ ARMY Editor/ViewModel/MainWindowViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ARMY_Editor.ViewModel
 {
@@ -26,6 +27,11 @@ namespace ARMY_Editor.ViewModel
                 return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
             }
         }
+
+        public ICommand AddToWar { get; set; }
+        public ICommand CallBackFromWar { get; set; }
+        public ICommand CreateNewTrooper { get; set; }
+
         public MainWindowViewModel()
         {
             this.Camp = new ObservableCollection<Trooper>();
