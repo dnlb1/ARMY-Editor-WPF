6e290c347eefb944a69a50e99494b78132072afb
Bencze Dániel <rgrengarhyped@gmail.com>
2023-02-15 14:54:57 +0100

    IsInDmode

--- ARMY Editor/ViewModel/MainWindowViewModel.cs
+++ ARMY Editor/ViewModel/MainWindowViewModel.cs
@@ -3,9 +3,11 @@ using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ARMY_Editor.ViewModel
 {
@@ -15,5 +17,14 @@ namespace ARMY_Editor.ViewModel
         ObservableCollection<Trooper> Camp { get; set; }
         //War
         ObservableCollection<Trooper> War { get; set; }
+
+        public bool IsInDesigner
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
     }
 }
